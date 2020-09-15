eb25ec75d728d6b9801e47d4ffa33da6360ee6b4
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-09-15 23:00:00 +0300

    Add the comment for the syncutils.WaitGroup interface

--- wait_group.go
+++ wait_group.go
@@ -2,7 +2,8 @@ package syncutils
 
 //go:generate mockery -name=WaitGroup -inpkg -case=underscore -testonly
 
-// WaitGroup ...
+// WaitGroup represents the interface of the sync.WaitGroup type.
+// It might be useful for supporting the ability to mock the latter.
 type WaitGroup interface {
 	Add(delta int)
 	Done()
