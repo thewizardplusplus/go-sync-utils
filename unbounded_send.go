706b71aa2097e64cceccd6d52857d2e65c1d8223
thewizardplusplus <thewizardplusplus@yandex.ru>
2020-09-19 23:00:00 +0300

    Add the comment for the syncutils.UnboundedSend() function

--- unbounded_send.go
+++ unbounded_send.go
@@ -4,7 +4,11 @@ import (
 	"reflect"
 )
 
-// UnboundedSend ...
+// UnboundedSend sends the provided data to the provided channel
+// without blocking even if the channel is busy. This function works
+// with any types, but the data should have the type assignable to the type
+// of the channel item. The order of receiving data may not correspond
+// to the order of sending one.
 func UnboundedSend(channel interface{}, data interface{}) {
 	channelReflection := reflect.ValueOf(channel)
 	dataReflection := reflect.ValueOf(data)
