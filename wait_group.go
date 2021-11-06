09413258004fb2dfb91d8e007900685d4857cf3c
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-11-06 23:00:00 +0300

    Update the mock generation

--- wait_group.go
+++ wait_group.go
@@ -1,6 +1,6 @@
 package syncutils
 
-//go:generate mockery -name=WaitGroup -inpkg -case=underscore -testonly
+//go:generate mockery --name=WaitGroup --inpackage --case=underscore --testonly
 
 // WaitGroup represents the interface of the sync.WaitGroup type.
 // It might be useful for supporting the ability to mock the latter.
